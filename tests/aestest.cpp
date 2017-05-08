3a092d274eaeb57e7b56486a7adafc411718210a
NeverMine17 <dannevergame@gmail.com>
2017-05-08 22:48:38 +0400

    test

--- tests/aestest.cpp
+++ tests/aestest.cpp
@@ -5,30 +5,54 @@
 #include <cstring>
 #include <openssl/aes.h>
 
-int main()
-{
-    unsigned char inbuffer[1024];
-    unsigned char encryptedbuffer[1024];
-    unsigned char outbuffer[1024];
+typedef unsigned char buffer;
 
-    unsigned char oneKey[] = "hellonearth";
+char* encrypt_aes(const unsigned char oneKey[])
+{
+    buffer in[1024];
+    buffer encryptedbuffer[1024];
+    std::string straa = "hellonearth\n";
+
     AES_KEY key;
 
     AES_set_encrypt_key(oneKey,256,&key);
 
-    std::string straa = "hello world\n";
-    memcpy((char*)inbuffer,straa.c_str(),13);
+    memcpy((char*)in,straa.c_str(),13);
 
-    AES_encrypt(inbuffer,encryptedbuffer,&key);
+    AES_encrypt(in,encryptedbuffer,&key);
+    return (char*)encryptedbuffer;
+}
+
+char* decrypt_aes(buffer enc[1024], const unsigned char oneKey[])
+{
+    buffer temp[1024];
+
+    AES_KEY key;
 
     AES_set_decrypt_key(oneKey,256,&key);
 
-    AES_decrypt(encryptedbuffer,outbuffer,&key);
+    AES_decrypt(enc,temp,&key);
+    return temp;
+}
 
-    if ((char*)outbuffer == straa)
+int main()
+{
+    //buffer inbuffer[1024];
+    //buffer encryptedbuffer[1024];
+    buffer outbuffer[1024];
+
+    const unsigned char oneKey[] = {};
+
+    AES_KEY key;
+
+    AES_set_decrypt_key(oneKey,256,&key);
+
+    AES_decrypt(encrypt_aes("Data\n"),outbuffer,&key);
+
+    /*if ((char*)outbuffer == straa)
     {
         std::cout << "AES-256 test ok" << std::endl;
-    }
+    }*/
 
     getchar();
 
